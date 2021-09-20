75e752f50b4d6314d1fe036f738079ddde605c2e
JuWorkshop <89390048+JuWorkshop@users.noreply.github.com>
2021-09-20 06:07:13 +0200

    Add files via upload

---
+++ Src_Teensy3.2AntBmsToCan/Parameters.h
@@ -0,0 +1,5 @@
+uint16_t  ChargeVsetpoint = 2575;//  "Charge Voltage Limit (CVL)" = ChargeVsetpoint*NbrCell.
+uint16_t  chargecurrent = 500; //"Charge Current Limit (CCL)" : max charge current in 0.1A
+uint16_t  discurrent = 750; //"Discharge Current Limit (DCL)" : max discharge current in 0.1A
+uint16_t  MaxCellVoltage = 2575; //: max cell voltage in mv
+uint16_t  DischVsetpoint = 2000;//  
